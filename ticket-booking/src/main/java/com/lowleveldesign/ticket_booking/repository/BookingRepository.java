29b6d556eb84d16721c43e228c2b4885471415db
DevanshuTiwari <devanshu1336@gmail.com>
2026-03-20 12:02:36 +0530

    add query to find bookings with status pending to handle TTL

--- ticket-booking/src/main/java/com/lowleveldesign/ticket_booking/repository/BookingRepository.java
+++ ticket-booking/src/main/java/com/lowleveldesign/ticket_booking/repository/BookingRepository.java
@@ -1,10 +1,16 @@
 package com.lowleveldesign.ticket_booking.repository;
 
 import com.lowleveldesign.ticket_booking.entity.Booking;
+import com.lowleveldesign.ticket_booking.enums.BookingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    List<Booking> findBookingByStatusAndCreatedAt(BookingStatus bookingStatus, LocalDateTime time);
 
 }
